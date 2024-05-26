3f5f05261886929f9b527412aeb3cecaa10fcfc1
inert14 <cyberpunk.inert14@gmail.com>
2024-05-26 22:43:56 +0530

    Add option headless

--- SendWhatsappWithSelenium.py
+++ SendWhatsappWithSelenium.py
@@ -21,7 +21,7 @@ def send_message(phone_no, message):
     else: 
         user_data_dir = f"user-data-dir={script_directory}/userdata"
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
