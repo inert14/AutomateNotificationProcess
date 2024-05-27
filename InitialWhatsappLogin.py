904ff08c6443698b89719f26a45cfe622efcdb17
inert14 <cyberpunk.inert14@gmail.com>
2024-05-27 17:08:10 +0530

    Add chromedriver options

--- InitialWhatsappLogin.py
+++ InitialWhatsappLogin.py
@@ -39,6 +39,12 @@ def initial_login():
     else:
         user_data_dir = f"user-data-dir={script_directory}/userdata"
     options.add_argument(user_data_dir)  # Save session data
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-setuid-sandbox")
+    options.add_argument("--remote-debugging-port=9222")
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(service=service, options=options)
 
